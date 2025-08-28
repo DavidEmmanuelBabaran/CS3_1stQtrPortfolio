1933c4e344c9870f5f71bc8af37d9d6da95d4474
David Emmanuel Babaran <b2029devbabaran@pshs.edu.ph>
2025-08-28 16:10:53 +0800

    o

---
+++ public/anything.js
@@ -0,0 +1,18 @@
+console.log ("Hala Madrid");
+
+//rma - 15
+
+rl = require('readline')
+rli = rl.createInterface({
+    input: process.stdin,
+})
+rli.on('line', give_station)
+
+function give_station (num){
+    switch (Number(num)) {
+        case 15: 
+            console.log('rma' , ' -15')
+            break
+
+    }
+}
