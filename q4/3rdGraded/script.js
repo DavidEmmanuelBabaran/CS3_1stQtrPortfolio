1b80a28bf06fcc5fc25e9b4fe039f39507b5fe9d
DavidEmmanuelBabaran <b2029devbabaran@pshs.edu.ph>
2026-03-31 22:06:43 +0800

    1

--- q4/3rdGraded/script.js
+++ q4/3rdGraded/script.js
@@ -4,20 +4,20 @@ function plotPoint() {
     var point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.style.left = (x.value - x0.value + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.style.bottom = (y.value - y0.value + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = Number(x.value) - Number(x0.value) + 200 - 5 + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = Number(y.value) - Number(y0.value) + 200 - 5 + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x.value > x0.value && y.value > y0.value){
+    if (Number(x.value) > Number(x0.value) && Number(y.value) > Number(y0.value)){
       out1.innerHTML = "NE"
     }
-    else if (x.value < x0.value && y.value < y0.value){
+    else if (Number(x.value) < Number(x0.value) && Number(y.value) < Number(y0.value)){
       out1.innerHTML = "SO"
     }
-    else if (x.value > x0.value && y.value < y0.value){
+    else if (Number(x.value) > Number(x0.value) && Number(y.value) < Number(y0.value)){
       out1.innerHTML = "SE"
     }
-    else if (x.value < x0.value && y.value > y0.value){
+    else if (Number(x.value) < Number(x0.value) && Number(y.value) > Number(y0.value)){
       out1.innerHTML = "NO"
     }
     else{
